6909509bdc80ddfcbdf482ec24aa781ca1a47793
Niels <niels.hoffmann@hotmail.com>
2026-07-02 10:03:16 +0200

    MVP examples working

--- build.sh
+++ build.sh
@@ -12,3 +12,6 @@ fi
 docker run -it --pull=always --rm --workdir /workspace -v "$(pwd):/workspace" ${VOLUMES} \
   ghcr.io/opengeospatial/bblocks-postprocess \
   --clean true --base-url http://localhost:9090/register/
+# docker run -it --rm --workdir /workspace -v "$(pwd):/workspace" ${VOLUMES} \
+#   ghcr.io/opengeospatial/bblocks-postprocess \
+#   --clean true --base-url http://localhost:9090/register/
